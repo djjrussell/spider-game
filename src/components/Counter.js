8daa39b51423fc62220657ec55d6614b9cdaaf51
djjrussell <djjrussell@gmail.com>
2019-07-18 19:02:20 -0700

    counter is working as expected, score keeping is working, need to refactor

---
+++ src/components/Counter.js
@@ -0,0 +1,61 @@
+import React from 'react'
+
+export class Counter extends React.Component {
+
+    constructor(props) {
+
+        super(props);
+
+        this.state = {
+
+            time: 0,
+            offset: Date.now()
+
+        };
+
+        this.start = this.start.bind(this);
+        this.update = this.update.bind(this);
+        this.delta = this.delta.bind(this);
+
+        this.start();
+    }
+
+    delta() {
+
+        let now = Date.now();
+        return now - this.state.offset;
+
+    }
+
+    isOver(){
+
+        return this.state.time <= 0;
+
+    }
+
+    update() {
+        let newTime = 16 - Math.floor(this.delta() / 1000);
+        this.setState({time: newTime});
+
+        if(this.isOver()){
+
+            document.dispatchEvent(new Event("gameOver"));
+
+        }
+    }
+
+    start() {
+
+        setInterval(this.update, 1000);
+
+    }
+
+
+    render() {
+        return (
+            <div id="counter">
+                {this.state.time}
+            </div>
+        )
+    }
+};
--- src/components/Game.js
+++ src/components/Game.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {StartButton} from './StartButton.js'
 import {Board} from "./Board";
 import {ScoreDisplay} from "./ScoreDisplay.js";
+import {Counter} from "./Counter.js";
 
 export class Game extends React.Component {
 
@@ -9,21 +10,25 @@ export class Game extends React.Component {
         super(props);
 
         this.state = {
+            counter: null,
             gameSpace: null,
             score: 0
         };
 
         this.handleButtonClick = this.handleButtonClick.bind(this);
         this.setScore = this.setScore.bind(this);
+        this.gameOver = this.gameOver.bind(this);
 
         document.addEventListener("bugEaten", this.setScore);
+        document.addEventListener("gameOver", this.gameOver);
 
     }
 
     handleButtonClick() {
 
         this.setState({
-            gameSpace: <Board/>
+            gameSpace: <Board/>,
+            counter: <Counter/>
         })
 
     }
@@ -39,12 +44,21 @@ export class Game extends React.Component {
         );
     }
 
+    gameOver() {
+        document.getElementById("counter").remove();
+        alert("YOU SCORED " + this.state.score);
+        document.location = document.location;
+    }
+
     render() {
         return (
             <div id="GAME">
                 {/*<div id="header">*/}
-                    < StartButton className='button' onClick={this.handleButtonClick}/>
-                    < ScoreDisplay score={this.state.score}/>
+                < StartButton className='button' onClick={this.handleButtonClick}/>
+                <div>
+                    {this.state.counter}
+                </div>
+                < ScoreDisplay score={this.state.score}/>
                 {/*</div>*/}
                 <div>
                     {this.state.gameSpace}
